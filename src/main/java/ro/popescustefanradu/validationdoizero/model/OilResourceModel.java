69624d1166462bc4de6dca29317405aca704c8c2
stefan.popescu <stefan.popescu@esolutions.ro>
2019-07-20 20:20:08 +0300

    Some more test crap

--- src/main/java/ro/popescustefanradu/validationdoizero/model/OilResourceModel.java
+++ src/main/java/ro/popescustefanradu/validationdoizero/model/OilResourceModel.java
@@ -2,9 +2,11 @@ package ro.popescustefanradu.validationdoizero.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class OilResourceModel {
@@ -23,4 +25,7 @@ public class OilResourceModel {
 
     @NotNull
     Boolean inProduction;
+
+    @NotEmpty
+    List<@NotEmpty String> legalConditions;
 }
